79e5d8784e276a80ade684cd5a55b2df3df33d32
QuocAn108 <anhoang800140@gmail.com>
2026-04-13 21:28:16 +0700

    refactor: remove back home link from login form description

--- src/features/auth/pages/login.tsx
+++ src/features/auth/pages/login.tsx
@@ -588,14 +588,7 @@ const LoginPage = () => {
                 <p className="text-gray-500 text-base">
                   {t('AuthPages.login.loginForm.description')}
                 </p>
-                <div>
-                  <Link
-                    to={toLocalizedAuthPath('/')}
-                    className="inline-flex items-center text-sm font-medium text-[#1F85F5] hover:text-[#00d1c0] transition-colors"
-                  >
-                    {t('AuthPages.login.loginForm.backHome')}
-                  </Link>
-                </div>
+                <div></div>
               </div>
 
               {/* Form */}
